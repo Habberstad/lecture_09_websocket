7d3cb4015f0e1dda312f80040d22920fcab1ef93
Kristoffer Habberstad <kristoffer.habberstad@gmail.com>
2022-03-24 23:05:05 +0100

    Init commit

---
+++ client/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+    return <div><h1>Hello World!</h1></div>;
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
